ef9a9a330a0d9301a2a55dbf9277cc43978737ad
nitinkumar <nitinkumar18ad@gmail.com>
2026-04-14 21:47:42 +0530

    14-4-26

--- Lectures/Lecture36/QuickSort.cpp
+++ Lectures/Lecture36/QuickSort.cpp
@@ -55,8 +55,8 @@ int main() {
     quickSort(arr,0,n-1);
 
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }cout<<endl;
+        cout << arr[i]<<" ";
+    }cout << endl;
 
     return 0;
 }
