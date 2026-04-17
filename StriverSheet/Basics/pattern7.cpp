0956aafdc4f677450991e984c55de0c304024a03
nitinkumar <nitinkumar18ad@gmail.com>
2026-04-17 21:07:18 +0530

    pattern

--- StriverSheet/Basics/pattern7.cpp
+++ StriverSheet/Basics/pattern7.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void pattern(int n){
     for(int i = 1; i<=n ; i++){
         for(int j=1;j<=n-i;j++){
-            cout<<"_";
+            cout<<" ";
             }   
             for(int j=1;j<=2*i-1;j++){
                 cout<<"*";
