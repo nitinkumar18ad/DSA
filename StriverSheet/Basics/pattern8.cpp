a95d4f2c7cc165910933c419712ff4ecb1ca5d2f
nitinkumar <nitinkumar18ad@gmail.com>
2026-04-18 17:44:17 +0530

    pattern

--- StriverSheet/Basics/pattern8.cpp
+++ StriverSheet/Basics/pattern8.cpp
@@ -9,7 +9,6 @@ void pattern(int n){
         for(int j=1;j<=2*(n-i)+1;j++){
             cout<<"*";
         }
-        
         cout<<endl;
     }
 }
--- StriverSheet/Basics/pattern9.cpp
+++ StriverSheet/Basics/pattern9.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 
 void pattern(int n){
-    for(int i =1;i<n;i++){
-        for(int j=1;j<n-i;j++){
+    for(int i =1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
             cout<<" ";
-        }
-        for(int j=1;j<2*i-1;j++){
-            cout<<"*";
-        }
-        cout<<endl;
+            }   
+            for(int j=1;j<=2*i-1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+    }
+
+     for(int i= 1 ;i<=n;i++){
         for(int j=1;j<=i-1;j++){
             cout<<" ";
         }
-        for(int j=1;j< 2*(n-i)+1;j++){
+        for(int j=1;j<=2*(n-i)+1;j++){
             cout<<"*";
         }
         cout<<endl;
