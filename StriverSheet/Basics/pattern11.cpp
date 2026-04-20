28cbc5a9b8fd08cc8f00d3a5590bff158fb316c2
nitinkumar <nitinkumar18ad@gmail.com>
2026-04-20 21:23:57 +0530

    pattern

--- StriverSheet/Basics/pattern11.cpp
+++ StriverSheet/Basics/pattern11.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 void pattern(int n){
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<i;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
             if((i+j)%2 == 0)
             cout<< 1 ;
             else
