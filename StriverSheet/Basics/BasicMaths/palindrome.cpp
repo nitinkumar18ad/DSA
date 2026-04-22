17fa509f474c5d46242feda10ba03d2c3efafbda
nitinkumar <nitinkumar18ad@gmail.com>
2026-04-22 23:23:57 +0530

    Palindrome

---
+++ StriverSheet/Basics/BasicMaths/palindrome.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+bool Palindrome(int n){
+    int original = n;
+    int rev = 0;
+
+    while(n>0){
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+    return original == rev;
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>> n;
+    
+    if(Palindrome(n))
+        cout<<"True";
+    else
+        cout<<"False";
+}
