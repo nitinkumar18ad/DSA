2c33429068dbe7f579ba189bc80aef3a8cafa19e
nitinkumar <nitinkumar18ad@gmail.com>
2026-04-25 01:43:49 +0530

    gcd optimal

--- StriverSheet/Basics/BasicMaths/gcd.cpp
+++ StriverSheet/Basics/BasicMaths/gcd.cpp
@@ -1,14 +1,28 @@
 #include<iostream>
 using namespace std;
 
+
+//BruteForce:
+// int gcd(int a,int b){
+//     int ans = 0;
+//     for(int i = 1;i<min(a,b);i++){
+//         if(a%i == 0 && b%i == 0)
+//             ans = i;
+//     }
+//     return ans;
+//     }
+
+//Optimal:
 int gcd(int a,int b){
-    int ans = 0;
-    for(int i = 1;i<min(a,b);i++){
-        if(a%i == 0 && b%i == 0)
-            ans = i;
+    while(b!=0){
+        int temp = b;
+        b = a% b;
+        a = temp;
+
     }
-    return ans;
-    }
+    return a;
+
+}
 
     int main(){
         int n1,n2;
