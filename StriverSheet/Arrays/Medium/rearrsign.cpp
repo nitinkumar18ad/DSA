52104023946572e76410b190477413c891550538
nitinkumar <nitinkumar18ad@gmail.com>
2026-06-25 18:42:13 +0530

    rearrage sign

--- StriverSheet/Arrays/Medium/rearrsign.cpp
+++ StriverSheet/Arrays/Medium/rearrsign.cpp
@@ -3,7 +3,32 @@
 
 using namespace std;
 
-int rearr(vector<int>& nums ){
-    
+vector<int> rearr(vector<int>& nums ){
+    vector<int> ans(nums.size());
 
+    int posIndex = 0;
+    int negIndex = 1;
+
+    for(int i= 0; i<nums.size();i++){
+        if(nums[i] > 0){
+            ans[posIndex] = nums[i];
+            posIndex += 2;
+        }
+        else {
+            ans[negIndex] = nums[i];
+            negIndex += 2;
+        }
+    }
+return ans;
+}
+
+int main(){
+    vector<int> nums = {-2,3,-1,5,9,-7};
+
+    vector<int> ans = rearr(nums);
+    
+    for(int i =0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    return 0;
 }
