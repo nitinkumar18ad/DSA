6d532b494028a66d84ba2dff604141149ccef423
nitinkumar <nitinkumar18ad@gmail.com>
2026-07-01 23:50:36 +0530

    Daily commit

---
+++ StriverSheet/Arrays/Easy/nextPer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> nextPer(vector<int>& nums) {
+    int n = nums.size();
+    int index = -1;
+    
+    for(int i = 0;i<nums.size();i++){
+        if(i<n){
+            
+        }
+
+    }
+
+}
