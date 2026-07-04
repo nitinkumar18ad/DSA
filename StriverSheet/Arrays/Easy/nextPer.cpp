402660152a7c8a9280c578e945763acf81416189
nitinkumar <nitinkumar18ad@gmail.com>
2026-07-04 06:00:07 +0530

    next  Permutation

--- StriverSheet/Arrays/Easy/nextPer.cpp
+++ StriverSheet/Arrays/Easy/nextPer.cpp
@@ -1,17 +1,42 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
-vector<int> nextPer(vector<int>& nums) {
+void nextPer(vector<int>& nums) {
     int n = nums.size();
     int index = -1;
     
-    for(int i = 0;i<nums.size();i++){
-        if(i<n){
-            
+    for(int i = n-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
+            index = i;
+            break;
         }
 
+    if(index == -1){
+        reverse(nums.begin(),nums.end());
+        return;
     }
 
+    for(int i = n-1;i>index;i--){
+        if(nums[i]>nums[index]){
+            swap(nums[i],nums[index]);
+            break;
+        }
+    }
+
+    reverse(nums.begin()+index+1,nums.end());
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 2, 5, 3, -6};
+    nextPer(nums);
+    cout << "Next Permutation: ";
+
+    for (int x : nums)
+        cout << x << " ";
+
+    return 0;
 }
