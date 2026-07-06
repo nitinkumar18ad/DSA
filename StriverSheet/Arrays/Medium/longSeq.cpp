93420f094bebec47a315368e382bfe745d2b4021
nitinkumar <nitinkumar18ad@gmail.com>
2026-07-06 08:36:11 +0530

    longest sequence

---
+++ StriverSheet/Arrays/Medium/longSeq.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int longestConsecutive(vector<int>& nums){
+    if(nums.empty()) return 0;
+
+    sort(nums.begin(),nums.end());
+
+    int longest = 1;
+    int count = 1;
+
+    for(int i = 1; i<nums.size();i++){
+        if(nums[i] == nums[i-1] + 1){
+            count ++;
+        }
+        else if (nums[i] != nums[i-1]){
+            count = 1;
+        }
+
+        longest = max(longest,count);
+    }
+
+    return longest;
+}
+
+int main() {
+    vector<int> nums = {1, 33, 2, 66, 89, 4, 5};
+
+    int longest = longestConsecutive(nums);
+
+    cout << "Longest Consecutive Sequence: " << longest << endl;
+
+    return 0;
+}
