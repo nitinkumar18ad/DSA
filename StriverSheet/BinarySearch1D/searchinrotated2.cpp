5f21d748dbec797354249fa3da66b29d8260b843
nitinkumar <nitinkumar18ad@gmail.com>
2026-07-17 15:49:28 +0530

    search in Rotated Array 2

--- StriverSheet/BinarySearch1D/searchinrotated2.cpp
+++ StriverSheet/BinarySearch1D/searchinrotated2.cpp
@@ -3,35 +3,42 @@
 
 using namespace std;
 
-int search(vector<int>& nums,int target){
+bool search(vector<int> & nums,int target){
     int low = 0,high = nums.size()-1;
-    
+
     while(low <= high){
         int mid = low + (high-low)/2;
 
         if(nums[mid] == target)
-            return mid;
+            return true;
+
+        if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+            low++;
+            high--;
+            continue;
+        }
 
         if(nums[low] <= nums[mid]){
             if(nums[low] <= target && target < nums[mid])
                 high = mid-1;
-
             else
-                low = mid +1;
+                low = mid + 1;
         }
         else{
-            if(nums[mid] < target && target <= nums[high])
+            if(nums[mid]< target && target <= nums[high])
                 low = mid + 1;
-            else 
+            else
                 high = mid - 1;
         }
     }
-    return -1;
+    return false;
 }
 
 int main(){
-    vector<int> nums = {4,5,6,1,2};
-    int target = 1;
+    vector<int> nums = {5,6,7,1,2,4};
+    int target = 2;
 
-    cout<<"Searched:"<<search(nums,target);
+    cout<<"Search :"<<search(nums,target);
+
+    return 0;
 }
