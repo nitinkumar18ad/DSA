4b2b41d66e79429c09a9a9d330c79ea7aac39770
nitinkumar <nitinkumar18ad@gmail.com>
2026-09-17 17:12:23 +0530

    move zeros at the end

---
+++ practice/movezeros.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int arr[] = {0, 1, 0, 3, 12};
+int pos = 0;
+int n = sizeof(arr)/sizeof(arr[0]);
+
+for(int i =0;i<n;i++){
+if(arr[i] != 0){
+arr[pos] = arr[i];
+pos++;
+}
+
+for(int i =pos;i<n;i++){
+arr[i] = 0;
+}
+
+for(int i =0;i<n;i++){
+cout<<arr[i]<<" ";
+}
+}
+}
