37fc18f4c3f43021f897db4a4cc7188c057f81b2
nitinkumar <nitinkumar18ad@gmail.com>
2026-09-17 21:24:48 +0530

    String

--- practice/movezeros.cpp
+++ practice/movezeros.cpp
@@ -3,22 +3,20 @@
 using namespace std;
 
 int main(){
-int arr[] = {0, 1, 0, 3, 12};
-int pos = 0;
-int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {0, 1, 0, 3, 12};
+    int pos = 0;
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-for(int i =0;i<n;i++){
-if(arr[i] != 0){
-arr[pos] = arr[i];
-pos++;
-}
-
-for(int i =pos;i<n;i++){
-arr[i] = 0;
-}
-
-for(int i =0;i<n;i++){
-cout<<arr[i]<<" ";
-}
-}
+    for(int i = 0;i<n;i++){
+        if(arr[i] != 0){
+            arr[pos] = arr[i];
+            pos++;
+        }
+    }
+    for(int i = pos;i<n;i++){
+        arr[i] = 0;
+    }
+    for(int i =0;i<n;i++){
+        cout<<arr[i] <<" ";
+    }
 }
---
+++ practice/string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+string s = "hello";
+
+for(int i =0;i<s.size();i++){
+cout<<s[i]<<endl;
+}
+}
